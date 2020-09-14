3433acc7b2fc030eea45fe189ffc5d73a9faf89c
Zain Noman <zaina526@gmail.com>
2020-09-14 19:15:30 +0500

    removed video capture being read twice

--- detectMarkers.py
+++ detectMarkers.py
@@ -25,15 +25,14 @@ def testArUcoDetection():
         return
     cv2.imshow("WINNAME",img)
 
-def getMarkerTransform():
-    ref,img = vid.read()
+def getMarkerTransform(img):
     corners,ids,_ = cv2.aruco.detectMarkers(img,dictionary)
 
     if(ids is not None):
         rvecs,tvecs,_ = cv2.aruco.estimatePoseSingleMarkers(corners,12.75,cameraMatrix,distortionCoeffs)
         dst,_ = cv2.Rodrigues(rvecs[0])
         fwdVector= np.matmul( dst,np.array([0,0,1]) )
-        pointer = np.matmul( dst,np.array([0,9,0]) ) + tvecs[0]
+        pointer = np.matmul( dst,np.array([0,9,0]) ) + tvecs[0]     #as sights are on top of marker
         return pointer, fwdVector
     else:
         return None,None
@@ -44,11 +43,17 @@ def getPointOfIntersectionLines():
     while(P1 is None):
         print("P1 not set, presss Enter when you want to calibrate") 
         input()
-        P1,V1 = getMarkerTransform()
+        img=[]
+        while(img.length==0):
+            _,img = vid.read()
+        P1,V1 = getMarkerTransform(img)
     while(P2 is None):
         print("P2 not set, presss Enter when you want to calibrate") 
         input()
-        P2,V2 = getMarkerTransform()
+        img=[]
+        while(img.length==0):
+            _,img = vid.read()
+        P2,V2 = getMarkerTransform(img)
     V3 =  np.cross(V1,V2)
     VMat = np.array( [ [V1[0],V1[1],V1[2]] , [V2[0],V2[1],V2[2]] , [V3[0],V3[1],V3[2]]  ])
     tVec = np.matmul( P2-P1 ,np.linalg.inv(VMat))
@@ -84,7 +89,7 @@ TL= np.array([18,0,0])
 print("Screen points positions: ",BL,BR,TL)
 
 mouse = Controller()
-print("take mouse to bottom roight and press enter")
+print("take mouse to bottom right and press enter")
 input()
 xMax,yMax = mouse.position
 print(xMax,yMax)
@@ -93,32 +98,41 @@ xArray= np.zeros(10)
 yArray= np.zeros(10)
 sos = signal.butter(10,0.35,output='sos')
 
-LastFrameTime=0
+LastFrameTime=2
 while True:
     #testArUcoDetection()
     _,img = vid.read()
-    if (showVid):
-        cv2.imshow("yo", img)
-    if (len(img)==0):
-        print("Empty frame detected")
+    if (img is None):
+        print("img is none")
         break
-    if (cv2.waitKey(5)>0):
+    if (img.size==0):
+        print("empty frame detected")
         break
 
-    x = xArray[9]
-    y = yArray[9]
-    P,Fwd =  getMarkerTransform()
+    getTransformStartTime = time.process_time()
+    P,Fwd =  getMarkerTransform(img)
+    print ( "getMarkerTransform time: "+str(time.process_time()-getTransformStartTime))
+    
+    
     if (P is not None):
+        getScreenposTime = time.process_time()
         x,y = getNoramlizedScreenpos(BL,BR,TL,P,Fwd)
-    xArray =  np.roll(xArray,-1)
-    xArray[0] = x
-    yArray =  np.roll(yArray,-1)
-    yArray[0] = y
+        print ( "getScreenpos time: "+str(time.process_time()-getScreenposTime))
+    else:
+        x= xArray[9]
+        y= yArray[9]
+
+    xArray =  np.roll(xArray,1)    #just right shifting the array and adding latest value of x
+    xArray[9] = x
+    yArray =  np.roll(yArray,1)
+    yArray[9] = y
     xFiltered = signal.sosfilt(sos,xArray)[9]
     yFiltered = signal.sosfilt(sos,yArray)[9]
+    
     mouse.position = (xFiltered*xMax, (1-yFiltered) *yMax)
-    print("FPS: ", 1/(time.time()-LastFrameTime), " Hz" )
-    LastFrameTime = time.time()
+    
+    print("FPS: ", 1/(time.process_time()-LastFrameTime+0.00001), " Hz" )   # the 0.0001 is added to remove division by zero errors
+    LastFrameTime = time.process_time()
 
     
 cv2.destroyAllWindows()
